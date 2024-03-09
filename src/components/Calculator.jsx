694b4d6afe414905b3d979b5cef55a246c2cbcce
Youssef ait elhaj <youssef.aitelhaj.abdeslam@gmail.com>
2024-03-09 18:51:01 +0100

    init commit

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,10 @@
+import OutputScreen from "./OutputScreen";
+
+
+export default function Calculator() {
+    return (
+        <div className="calc">
+            <OutputScreen />
+        </div>
+    )
+}
---
+++ src/components/OutputScreen.jsx
@@ -0,0 +1,9 @@
+
+
+export default function OutputScreen() {
+    return (
+        <div className="output-scr">
+            <p className="output">15</p>
+        </div>
+    );
+}
